7987f99c62a857c9cc82c25a72bc07255b9f8b84
taha200 <41546947+taha200@users.noreply.github.com>
2023-05-24 16:40:06 +0500

    created doc types and tested

---
+++ sowaan_restaurant/sowaan_restaurant/doctype/sowaan_tablet_pos_settings/sowaan_tablet_pos_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Sowaan Private Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sowaan Tablet POS Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ sowaan_restaurant/sowaan_restaurant/doctype/stations/test_stations.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Sowaan Private Limited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestStations(FrappeTestCase):
+	pass
